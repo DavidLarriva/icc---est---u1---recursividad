b373677557e05425b2c7db7b76f41e587634f24f
David Larriva <dalarrivac@gmail.com>
2024-10-28 13:01:40 -0500

    Terminado

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Recursividad recursividad = new Recursividad();
+        
+        int suma = recursividad.sumaConsecutivos(10);
+        }
+}
--- Recursividad.java
+++ Recursividad.java
@@ -1,14 +1,30 @@
 public class Recursividad {
-    public static void main(String[] args) {
-        System.out.println("Factorial de 5: " + factorial(5));
-        }
+    
+        
         public static int factorial(int n) {
-            if (n == 0) {
+            System.out.println("Calculando el factorial de: " + n);
+            if (n == 0 || n==1)  {
+                System.out.println("El factorial de " + n + " es 1");
                 return 1;
                 } else {
-                    return n * factorial(n - 1);
+                    int resultado =  n * factorial(n - 1);
+                    System.out.println("El factorial de " + n + " es " + resultado);
+                    return resultado;
                     }
                     }
+
+        //Calcular la suma de los numeros consecutivos            
+        public int sumaConsecutivos(int n){
+            System.out.println("Calculando la suma de: " + n);
+            if (n == 0 || n==1)  {
+                System.out.println("La suma de " + n + " es 1");
+                return 1;
+                } else {
+                    int resultado =  n + sumaConsecutivos(n - 1);
+                    System.out.println("La suma de " + n + " es " + resultado);
+                    return resultado;
+                    }
+        }
                     
 
     }
